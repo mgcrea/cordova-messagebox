9ebf357682c3c26a7a273750c81bfd0a363fe0d3
Olivier Louvignes <olivier@mg-crea.com>
2012-11-04 14:22:34 +0100

    update(ios): updated demo plugin javascript

--- samples/ios/www/plugin.js
+++ samples/ios/www/plugin.js
@@ -1,39 +1,39 @@
 
-var plugin = {
+    var plugin = {
 
-    alert: function() {
-        window.plugins.messageBox.alert({title: 'Title', message: 'Message'}, function(button) {
-            var args = Array.prototype.slice.call(arguments, 0);
-            console.log("messageBox.alert:" + JSON.stringify(args));
-        });
-    },
+        alert: function() {
+            window.plugins.messageBox.alert({title: 'Title', message: 'Message'}, function(button) {
+                var args = Array.prototype.slice.call(arguments, 0);
+                console.log("messageBox.alert:" + JSON.stringify(args));
+            });
+        },
 
-    confirm: function() {
-        window.plugins.messageBox.confirm({title: 'Title', message: 'Message'}, function(button) {
-            var args = Array.prototype.slice.call(arguments, 0);
-            console.log("messageBox.confirm:" + JSON.stringify(args));
-        });
-    },
+        confirm: function() {
+            window.plugins.messageBox.confirm({title: 'Title', message: 'Message'}, function(button) {
+                var args = Array.prototype.slice.call(arguments, 0);
+                console.log("messageBox.confirm:" + JSON.stringify(args));
+            });
+        },
 
-    prompt : function() {
-        window.plugins.messageBox.prompt({title: 'Title', message: 'Message'}, function(button, value) {
-            var args = Array.prototype.slice.call(arguments, 0);
-            console.log("messageBox.prompt:" + JSON.stringify(args));
-        });
-    },
+        prompt : function() {
+            window.plugins.messageBox.prompt({title: 'Title', message: 'Message'}, function(button, value) {
+                var args = Array.prototype.slice.call(arguments, 0);
+                console.log("messageBox.prompt:" + JSON.stringify(args));
+            });
+        },
 
-    promptPassword : function() {
+        promptPassword : function() {
 
-        var options = {
-            title: 'Please enter your password',
-            placeholder: 'password',
-            type: 'password'
+            var options = {
+                title: 'Please enter your password',
+                placeholder: 'password',
+                type: 'password'
+            }
+
+            window.plugins.messageBox.prompt(options, function(button, value) {
+                var args = Array.prototype.slice.call(arguments, 0);
+                console.log("messageBox.prompt:" + JSON.stringify(args));
+            });
         }
 
-        window.plugins.messageBox.prompt(options, function(button, value) {
-            var args = Array.prototype.slice.call(arguments, 0);
-            console.log("messageBox.prompt:" + JSON.stringify(args));
-        });
-    }
-
-};
+    };
